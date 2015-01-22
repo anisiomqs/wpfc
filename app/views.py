330bcfc68c8e7d5cd4f8a2c914a6b89dc23c1f4f
Anisio Marques Junior <anisiomarxjr@gmail.com>
2015-01-21 22:10:57 -0400

    Deletes unnecessary files

--- app/views.py
+++ app/views.py
@@ -1,6 +1,5 @@
 from flask import render_template, flash, redirect
 from app import app,api,db
-#from .forms import LoginForm
 from flask import redirect
 from app.tagger import tagger,chunker
 from .models import Entry
@@ -19,13 +18,13 @@ def process_tweets():
         tweet_parts = chunk_tweet(tweet.text)
         what = concat_words(tweet_parts['WHAT'])
         where = concat_words(tweet_parts['LOC'])
-        when = datetime.utcnow()
+        when = tweet.created_at
         involveds = concat_words(tweet_parts['INVOLVED'])
         reference = concat_words(tweet_parts['REFERENCE'])
         how_much = float(replace_comma(concat_words(tweet_parts['HOW_MUCH'])))
         classification = tweet_parts['CLASSIFICATION']
         entry = Entry(text=tweet.text,what=what,
-                      where=where,when=None,
+                      where=where,when=when,
                       involveds=involveds,reference=reference,
                       how_much=how_much,classification=classification)
         db.session.add(entry)
@@ -40,12 +39,6 @@ def concat_words(words):
 
 def replace_comma(value):
     return value.replace(',','.')
-
-def i_o(list_o, list_i):
-    if length(list_o) > 0:
-        return False
-    else:
-        return True
 
 def normalize(sent):
     return re.sub(r'(?!\$)\W+', ' ', sent.lower())
