c68c2876f42005d3068b91eb2397871aabd42213
Anisio Marques Junior <anisiomarxjr@gmail.com>
2015-01-21 21:22:23 -0400

    Improved .gitignore

--- db_repository/manage.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-from migrate.versioning.shell import main
-
-if __name__ == '__main__':
-    main(six='<module 'six' from '/home/anisio/developer/python/sources/wpfc/flask/lib/python3.4/site-packages/six.py'>')
